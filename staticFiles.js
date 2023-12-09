07231496fa569f004458e62644050cc88e915d9b
Saturn-VI <112597058+Saturn-VI@users.noreply.github.com>
2023-12-09 16:34:52 -0600

    hotfix
    should work

--- staticFiles.js
+++ staticFiles.js
@@ -2,7 +2,8 @@ var path = require('path');
 
 module.exports = function (app, db) {
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, 'docs', 'index.html'));
+        // res.sendFile(path.join(__dirname, 'docs', 'index.html'));
+        res.redirect('/login');
     });
 
     app.get('/IDE', function(req, res){
