269198d6c5b2fcbe4fa59c63c915f94468ae395a
Ianyourgod <ianlouishawthorne@gmail.com>
2024-01-09 15:54:51 -0600

    cool sidebar thing

--- server/static/home.js
+++
@@ -1,6 +0,0 @@
-module.exports = function ({app, getStatic}) {
-    app.get('/', function(req, res) {
-        res.sendFile(getStatic('docs/index.html'));
-        //res.redirect('/login');
-    });
-}
