e57a34b02e2054ae8e9befd609fbbf2ba5e626b7
Saturn-VI <112597058+Saturn-VI@users.noreply.github.com>
2024-01-25 14:49:18 -0600

    Update createAccount.js

--- server/api/accounts/createAccount.js
+++ server/api/accounts/createAccount.js
@@ -9,23 +9,23 @@ module.exports = function ({app, UserManager}) {
         }
 
         if (username.length < 3 || username.length > 16) {
-            res.send([false, "username"]);
+            res.send([false, "username_length"]);
             return;
         }
 
         if (password.length < 8 || password.length > 32) {
-            res.send([false, "password"]);
+            res.send([false, "password_length"]);
         }
 
         for (const char of username.toLowerCase()) {
             if (!LETTERS.includes(char) && !NUMBERS.includes(char) && char !== "_") {
-                res.send([false, "username"]);
+                res.send([false, "username_bad_text"]);
             }
         }
 
         for (const char of password.toLowerCase()) {
             if (!LETTERS.includes(char) && !NUMBERS.includes(char) && char !== "_") {
-                res.send([false, "password"]);
+                res.send([false, "password_bad_text"]);
                 return;
             }
         }
