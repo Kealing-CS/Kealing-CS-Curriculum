566647f12015073d88ee4db4f9de69392b330730
Mr.chicken <chicken.science.lab@outlook.com>
2024-04-24 15:59:30 -0600

    More work on adding cert genaration

--- security/newcert.js
+++ security/newcert.js
@@ -1,6 +1,19 @@
-module.exports = class certificate{
-    has
-    constructor(hashing,expration,options){
+var crypto = require("crypto");
+var forge = require("node-forge");
 
+module.exports = class certificate{
+    ips;
+    URIs;
+    expiration;
+    options;
+    /*
+    Exsample options:
+    {"countryName":"US","ST":"Texas","organizationName":"Kealing","Expration":{"notBefore":new Date(),"NotAfter":new Date(new Date.getTime() + 1000 * 60 * 60 * 24 * 10)}} (10 Days)
+    */
+    constructor(altIPs,altURIs,options){
+    this.ips = altIPs;
+    this.URIs = altURIs;
+    this.expiration = options.expiration;
+    delete options.expiration;
     }
 }
