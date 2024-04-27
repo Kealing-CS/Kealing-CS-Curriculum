53dcde96142914fdc7193843565b361bb34aaa97
Mr.chicken <chicken.science.lab@outlook.com>
2024-04-27 10:56:58 -0600

    Make https work, but not http ( I will fix this soon O

--- security/https.js
+++ security/https.js
@@ -1,65 +1,35 @@
 var https = require("https");
 var tls = require("tls");
 var http = require("http");
+//var { storeHTTPOptions } = require("_http_server.js")
 var ser = require("_http_server");
 var net = require("net");
 var fs = require("fs");
 module.exports.listen = function (app, port, callbackFunction) {
  var options = {
-   key: fs.readFileSync("./security/certificates/key.pem").toString(),
-   cert: fs.readFileSync("./security/certificates/cert.pem").toString(),
+   key: fs.readFileSync("./security/certificates/key.pem"),
+   cert: fs.readFileSync("./security/certificates/cert.pem"),
  };
- var server = https.createServer(options, app);
- var tcpserver = net.createServer();
+var tcpserver = net.createServer();
+var server = https.createServer(options,app);
  tcpserver.listen(port);
  callbackFunction();
  tcpserver.on("connection", (socket) => {
+    var sock = new tls.TLSSocket(socket, {  isServer: true,...options });
+    http._connectionListener.call(server,sock);
+    return
    console.log("connect");
    socket.once("data", (data) => {
      console.log("data");
      if (data[0] === 22) {
-        var context = tls.createSecureContext(options);
        //https
-       /*var tserver = tls.TLSSocket.call(server,{
-        noDelay: true,
-        // http/1.0 is not defined as Protocol IDs in IANA
-        // https://www.iana.org/assignments/tls-extensiontype-values
-        //       /tls-extensiontype-values.xhtml#alpn-protocol-ids
-        ALPNProtocols: ['http/1.1'],
-        ...options,
-      })*/
-       var tserver = new tls.TLSSocket(socket, {
-         isServer: true,
-         noDelay: true,
-         // http/1.0 is not defined as Protocol IDs in IANA
-         // https://www.iana.org/assignments/tls-extensiontype-values
-         //       /tls-extensiontype-values.xhtml#alpn-protocol-ids
-         ALPNProtocols: ["http/1.1"],
-        context
-       });
-       http._connectionListener(server,tserver);
-      // tserver.emit('secureConnection', );
-       tserver.write("ping");
-
-       socket.emit("data", data);
+     }else{
+        http._connectionListener.call(server,socket);
+        socket.emit("data", data);
      }
     
-     /*
-     Function.prototype.call(
-       tls.Server,
-       server,
-       {
-         noDelay: true,
-         // http/1.0 is not defined as Protocol IDs in IANA
-         // https://www.iana.org/assignments/tls-extensiontype-values
-         //       /tls-extensiontype-values.xhtml#alpn-protocol-ids
-         ALPNProtocols: ["http/1.1"],
-         ...options,
-       },
-       http._connectionListener
-     );*/
-     socket.emit("data", data);
-   });
+    
+});
  });
  //server.listen(port,callbackFunction);
 };
