c10e12c5fe61b9352c6189714a2c721a9d4258fe
Mr.chicken <chicken.science.lab@outlook.com>
2024-04-27 18:02:58 -0600

    Edits ( Not a useful name eh? )

--- server/tasks.js
+++ server/tasks.js
@@ -1,18 +1,38 @@
 var fs = require("fs");
-const Time = require("timers/promises");
-var liseners = [];
-/**
- * 
- * @param { Array} newc 
- * @param { Array } oldc 
- */
-function addLiseners(newc, oldc) {
-  newc = JSON.parse(newc);
-  oldc = JSON.parse(oldc);
-  var lisenersToAdd = newc.filter(value => !oldc.includes(value));
-  lisenersToAdd.forEach((value,i) => {
-    // Time is in ms
-   Time.setTimeout()
-  });
-}
-fs.watchFile("./db/tasks.json", addLiseners);
+module.exports.addListener = function (name, time, callBack) {
+  var parsed = JSON.parse();
+};
+/* 
+Database structure will be like:
+[{"name":object | string,"date":Date object,callback:string ( Will be evailed) }]
+*/
+module.exports.removeListener = function (by) {};
+module.exports.listen = function () {
+    var listeners = [];
+    var ldata = [];
+  fs.readFile("./db/tasks.json", addListeners);
+  fs.watchFile("./db/tasks.json", addListeners);
+  /**
+   *
+   * @param { Array } newc
+   * @param { Array } oldc
+   */
+  async function addListeners(newc, oldc) {
+    if (typeof oldc === "undefined") {
+      oldc = "[]";
+    }
+    newc = JSON.parse(newc);
+    oldc = JSON.parse(oldc);
+    var lisenersToAdd = newc.filter((value) => !oldc.includes(value));
+    lisenersToAdd.forEach((value, i) => {
+      // Time is in ms
+      var sleep = (ms) => new Promise((res) => setTimeout(ms, res));
+      var delay = (new Date(value.date).getMilliseconds()) - (new Date().getMilliseconds());
+      if(delay<1){
+
+      }else{
+        listeners.push(sleep((new Date(value.date).getMilliseconds()) - (new Date().getMilliseconds())));
+      }
+    });
+  }
+};
