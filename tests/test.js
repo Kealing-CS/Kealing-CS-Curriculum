3950a38f48b408c0f4de4569045e547e4bd7fcf3
Mr.chicken <chicken.science.lab@outlook.com>
2024-05-04 09:55:13 -0600

    Fix logic error in test.js

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 const testlogin = require('./testLogin')
 const testAssignments = require('./testAssignments')
 
-if (process.argv.indexOf("--reset") > -1 && process.argv.indexOf("-R") > -1) {
+if (process.argv.indexOf("--reset") > -1 || process.argv.indexOf("-R") > -1) {
     console.log("Resetting database...")
     fs.writeFileSync('./db/sensitivedata.db', '')
     fs.writeFileSync('./db/userdata.db', '')
